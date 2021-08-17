b7ec3575231c511f950bd63b8c6a71d32031fe88
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-08-18 00:33:17 +0530

    Faqs Container

--- le-netflix/src/components/accordion/index.js
+++ le-netflix/src/components/accordion/index.js
@@ -1,4 +1,13 @@
 import React, { createContext, useContext, useState } from "react";
+import {
+  Body,
+  Container,
+  Frame,
+  Header,
+  Inner,
+  Item,
+  Title,
+} from "./styles/accordion";
 
 const ToggleContext = createContext();
 
